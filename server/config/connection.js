5ad9091f82552f9978c1717523751a8a3e597e76
Andi <andrea.strickland1990@gmail.com>
2023-10-16 17:13:38 -0500

    updated config again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.01/book-search', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  //useNewUrlParser: true,
+ // useUnifiedTopology: true,
   //useCreateIndex: true,
   //useFindAndModify: false,
 });

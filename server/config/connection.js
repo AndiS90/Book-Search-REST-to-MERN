fdd7f4d97515fa8b86c65c0eed09a542e7fcd591
Andi <andrea.strickland1990@gmail.com>
2023-10-16 16:39:51 -0500

    updating connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.01/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  //useCreateIndex: true,
+  //useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;

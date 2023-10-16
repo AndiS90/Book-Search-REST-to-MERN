d9876ce35c59df62b1a9ad1180a1080fa86f2e60
Andi <andrea.strickland1990@gmail.com>
2023-10-16 16:05:57 -0500

    updated to working

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,17 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 // serve up react front-end in production
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+// });
+
+// serve up react front-end in production
+const root = path.join(__dirname,'..', '..', 'client', 'build')
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  //res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.sendFile('index.html', { root });
 });
+
+
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
+ //const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -24,18 +24,31 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+
+// // serve up react front-end in production
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
 // if we're in production, serve client/build as static assets
+const root = path.join(__dirname, '..', 'client', 'build')
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(root));
 }
 
 
 // serve up react front-end in production
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile('index.html', { root });
 });
 
-// app.use(routes);
+ //app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on  http://localhost:${PORT}`));
